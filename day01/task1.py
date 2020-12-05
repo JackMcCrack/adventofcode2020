e29769e74ca524f0814b139c4d92a8f1806d3d7a
JackMcCrack <jack@charon.lan>
2020-12-05 23:47:47 +0900

    cleanup of day01 code

--- day01/task1.py
+++ day01/task1.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-numbers=open("input", "r")
-read=[]
-for i in numbers:
-    i = int(i.strip())
-    read.append(i)
-    for j in read:
-        if i+j==2020:
-            print(i, '+', j, "=", 2020)
-            print(i, '*', j, "=", i*j)
-            break
+
+def read_input(filename):
+    read = []
+    lines=open(filename, "r")
+    for i in lines:
+        read.append(int(i.strip()))
+    return read
+
+if __name__ == '__main__':
+    f = read_input("input")
+    read = []
+    for i in f:
+        read.append(i)
+        for j in read:
+            if i+j==2020:
+                print(i, '+', j, "=", 2020)
+                print(i, '*', j, "=", i*j)
+                break
 
--- day01/task2.py
+++ day01/task2.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-numbers=open("input", "r")
-read=[]
-for i in numbers:
-    i = int(i.strip())
-    read.append(i)
-    for j in read:
-        for k in read:
-            if i+j+k==2020:
-                print(i, '+', j, '+', k, "=", 2020)
-                print(i, '*', j, '*', k, "=", i*j*k)
+
+def read_input(filename):
+    read = []
+    lines=open(filename, "r")
+    for i in lines:
+        read.append(int(i.strip()))
+    return read
+
+if __name__ == '__main__':
+    f = read_input("input")
+    read = []
+    done = False
+    for i in f:
+        read.append(i)
+        for j in read:
+            for k in read:
+                if i+j+k==2020:
+                    print(i, '+', j, '+', k, '=', 2020)
+                    print(i, '*', j, '*', k, '=', i * j * k)
+                    done = True
+                    break
+            if done:
                 break
 
