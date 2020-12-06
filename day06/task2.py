92cee78fe3cef33ef45c79c38e2d90ff6193c9fa
JackMcCrack <jack@charon.lan>
2020-12-06 14:59:21 +0900

    Day 6 Task 2: done

--- day06/task2.py
+++ day06/task2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import string
 
 def read_input(filename):
     read = []
@@ -8,7 +9,35 @@ def read_input(filename):
         read.append(i.strip())
     return read
 
+def counter(group):
+    q = []
+    for c in string.ascii_lowercase:
+        result = True
+        for x in group:
+            if c not in x:
+                result = False
+        if result:
+            print('hello')
+            q.append(c)
+    print(group, len(set(q)))
+    return len(set(q))
+
 if __name__ == '__main__':
     f = read_input("input")
+    questions = []
+    group = []
     for line in f:
-        a = 1
+        if line == "":
+            questions.append(counter(group))
+            group = []
+        else:
+            group.append(line)
+
+    questions.append(counter(group))
+
+    qsum = 0
+    for q in questions:
+        qsum += q
+    print(questions, qsum)
+
+
