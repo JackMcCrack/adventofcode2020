4c9a1cd6c106aef729165d3ca5fd7bb882342404
JackMcCrack <jack@charon.lan>
2020-12-10 18:31:17 +0900

    Day 10 task 2: done

--- day10/task2.py
+++ day10/task2.py
@@ -5,10 +5,29 @@ def read_input(filename):
     read = []
     lines=open(filename, "r")
     for i in lines:
-        read.append(i.strip())
+        read.append(int(i.strip()))
     return read
+
+
+knowpath = {}
+def findnext(f, i):
+    if i == len(f)-1:
+        return 1
+    if i in knowpath:
+        return knowpath[i]
+    ans = 0
+    for j in range(i+1, len(f)):
+        if f[j]-f[i]<=3:
+            ans += findnext(f, j)
+    knowpath[i] = ans
+    return ans
+
+
 
 if __name__ == '__main__':
     f = read_input("input")
-    for line in f:
-        a = 1
+    f.append(0)
+    f.sort()
+    print(f)
+    path = []
+    print(findnext(f, 0))
